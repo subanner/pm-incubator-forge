812e0eada1c374dfdad13cbe06717c22d777e3b7
Dorcus257 <tpals257@gmail.com>
2025-12-24 15:11:32 +0900

    feat: [이세민] javascript 기본 연산 [PMIF-x]

---
+++ SeMinLee/javascript/javascript-app/src/App.tsx
@@ -0,0 +1,16 @@
+import './App.css'
+import { CalculationExample } from './math_operation/CalculationExample'
+
+function App() {
+
+  return (
+    <>
+    <div>
+      {/*커스텀 태그 생성*/}
+      <CalculationExample />
+    </div>
+    </>
+  )
+}
+
+export default App
---
+++ SeMinLee/javascript/javascript-app/src/math_operation/CalculationExample.tsx
@@ -0,0 +1,25 @@
+//자바스크립트에서 함수형 객체를 만들 때 사용하는 패턴
+//1. export const 로 시작
+//2. 함수 이름
+//3. = () => {} 사용
+//4. 구동시킬 코드 작성
+//5. return() 내부에는 react
+
+export const CalculationExample = () => {
+  const add = 3 + 3;
+    const subtract = 10 - 5;
+    const multiply = 4 * 2;
+    const divide = 20 / 4;
+    const remainder = 10 % 3;
+
+    return (
+        <div>
+            <h3>연산 결과</h3>
+            <p>3 + 3 = {add}</p>
+            <p>10 - 5 = {subtract}</p>
+            <p>4 * 2 = {multiply}</p>
+            <p>20 / 4 = {divide}</p>
+            <p>10 % 3 = {remainder}</p>
+        </div>
+    )
+  }
