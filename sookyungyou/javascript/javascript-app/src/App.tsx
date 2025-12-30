124373c311beca8532eefda48769475d7aa52d91
soo315kr <soo6885435@gmail.com>
2025-12-30 15:28:45 +0900

    feat : [유수경] for 문 활용하여 배열값 합산하기 [PMIF-x]

--- sookyungyou/javascript/javascript-app/src/App.tsx
+++ sookyungyou/javascript/javascript-app/src/App.tsx
@@ -5,6 +5,8 @@ import { LogicalOperationExample } from './logical_operation/LogicalOperationExa
 import { ControlFlowIf } from './control_flow/ControlFlowIf'
 import {FirstProblem} from './problem/FirstProblem'
 import { ControlFlowSwitch } from './control_flow/ControlFlowSwitch'
+import { ControlFlowFor } from './control_flow/ControlFlowFor'
+import { ControlFlowForSummation } from './control_flow/ControlFlowForSummation'
 
 function App() {
 
@@ -24,6 +26,8 @@ function App() {
         <ControlFlowIf/>
         <FirstProblem/>
         <ControlFlowSwitch/>
+        <ControlFlowFor/>
+        <ControlFlowForSummation/>
       </div>
     </>
   )
--- sookyungyou/javascript/javascript-app/src/control_flow/ControlFlowSwitch.tsx
+++ sookyungyou/javascript/javascript-app/src/control_flow/ControlFlowSwitch.tsx
@@ -1,13 +1,13 @@
 export const ControlFlowSwitch = () => {
     let grade = Math.floor(Math.random() * 5.3) + 1
-    // Math.random()의 경우 0 <= random 값 < 1 (0에서 1 사이의 값)
+    // Math.random()은 0 이상 1 미만의 값
     // 0 <= random <= 0.9999999999999999999
-    // 0 <= random < 1 <- * 5.3
-    // 0 <= random <= 5.xxx  
-    // 0 <= random <= 6.xxx
-    // Math.floor()은 버림 (바닥함수)
-    // 1, 2, 3, 4, 5, 6
+    // Math.random() * 5.3은 범위를 늘림 (0  ~ 5.2999999)
+    // Math.floor(숫자)는 소수점 버림 (1.9 -> 1) (바닥함수)
+    // Math.floor()에 +1 하는 이유: Math.floor()는 0부터 시작 -> 1~6 만들기 위해
+    // 따라서 grade는 1, 2, 3, 4, 5, 6 중 하나
     // 1(SSS), 2(SS), 3(S), 4(A), 5(B), 6(심해)
+
     let gradeName
 
     switch (grade) {
@@ -23,6 +23,7 @@ export const ControlFlowSwitch = () => {
 
     return (
         <div>
+        {/* return 부분은 화면에서 보여주는 내용 */}
             <h3>javascript 제어문 (switch)</h3>
             
             <pre>{
